125d19edef79a03c33b53d6d6d8e8459596d91ac
sswirydo <szymsimon@gmail.com>
2021-12-10 22:11:27 +0100

    benchmark start

--- Makefile
+++ Makefile
@@ -3,18 +3,17 @@ all:
 
 szymon: 
 	clear
-	sudo /media/sf_Postgres-RangeTypes/scripts/compile-psql.sh /media/sf_Postgres-RangeTypes/scripts/psql-script.sql /media/sf_Postgres-RangeTypes/postgres-ramen/
-
-sushimon:
+	sudo /media/sf_Postgres-RangeTypes/scripts/compile-psql.sh /media/sf_Postgres-RangeTypes/postgres-ramen/ /media/sf_Postgres-RangeTypes/scripts/psql-script.sql
+szymon-timed:
 	clear
-	sudo /media/sf_Postgres-RangeTypes/scripts/compile-psql.sh /media/sf_Postgres-RangeTypes/scripts/psql-script.sql /media/sf_Postgres-RangeTypes/postgres-REL_13_STABLE/
+	sudo /media/sf_Postgres-RangeTypes/scripts/compile-psql.sh /media/sf_Postgres-RangeTypes/postgres-ramen/ /media/sf_Postgres-RangeTypes/benchmark/join/test_equal_tables 
 
 alex:
 	clear
-	sudo /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/scripts/compile-psql.sh /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/scripts/psql-script.sql /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/postgres-ramen 
+	sudo /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/scripts/compile-psql.sh /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/postgres-ramen /home/parallels/Desktop/Parallels_Shared_Folders/Postgres-RangeTypes/scripts/psql-script.sql 
 
 aure:
 	clear
-	sudo /home/ubuntu/Postgres-RangeTypes/scripts/compile-psql.sh /home/ubuntu/Postgres-RangeTypes/scripts/psql-script.sql /home/ubuntu/Postgres-RangeTypes/postgres-ramen
+	sudo /home/ubuntu/Postgres-RangeTypes/scripts/compile-psql.sh /home/ubuntu/Postgres-RangeTypes/postgres-ramen /home/ubuntu/Postgres-RangeTypes/scripts/psql-script.sql
 
 nico:
--- scripts/compile-psql.sh
+++ scripts/compile-psql.sh
@@ -1,67 +1,57 @@
 #!/bin/bash
 
-:'  Expected format:
-    sudo ./compile-pgsql.sh full_path_sql_script ramen_sql_root_path [official_sql_root_path] 
+:'  COMPILE-PSQL SCRIPT:
+    Expected format:
+    sudo ./compile-psql.sh ramen_sql_root_path (full_path_sql_script | full_path_sql_script)
 '
 
+START=$(date +%s.%N)
+TIME=$(date +%s.%N)
 
+SQL_SCRIPT=''
+SQL_INIT_SCRIPT=''
+SQL_VACUUM_SCRIPT=''
+SQL_SELECT_SCRIPT=''
 
+# How to measure time with bash. (might not work on some OSs)
 
-
-function main {
-    current_path=$(pwd)
-
-    if [ $# -eq 2 ] 
-    then
-        cd "$2"
-        make_pgsql || error_quit "make"
-        run_pgsql || error_quit "run"
-
-        # sudo rm ${RAMEN_PATH}/ramendata/sushiOUT.txt || echo "rip sushi rm"
-        # sudo cp /usr/local/pgsql/data/sushiOUT.txt ${RAMEN_PATH}/ramendata/ || echo "rip sushi cp"
-
-        cd "${current_path}"
-    fi
-
-    if [ $# -eq 3 ] 
-    then
-        cd "$3"
-        make_pgsql || error_quit "make"
-        run_pgsql || error_quit "run"
-        cd "${current_path}"
-    fi
+function elapsed_time {
+    echo $TIME;
+}
+function start_timer {
+    START=$(date +%s%N)
+}
+function stop_timer {
+    TIME=$((($(date +%s%N) - $START)/1000000))
 }
 
+
 function check_arguments {
-    echo $#
     if [ $# -eq 0 ] || [ $# -ge 4 ] 
     then
         echo "Incorrect program format."
-        echo "Expected: sudo ./compile-pgsql full_path_sql_script ramen_sql_root_path [official_sql_root_path]"
+        echo "Expected: sudo ./compile-psql.sh ramen_sql_root_path (full_path_sql_script | full_path_sql_script)"
         exit
     fi
 }
-
 function check_root {
     if [[ $EUID -ne 0 ]]
     then
         echo "Root required!"
-        echo "\t Run with: sudo ./compile-pgsql full_path_sql_script ramen_sql_root_path [official_sql_root_path]"
+        echo "\t Run with: sudo ./compile-psql.sh ramen_sql_root_path (full_path_sql_script | full_path_sql_script)"
         exit
     fi
 }
-
 function error_quit {
     echo "Error during $1"
     exit
 }
 
 function make_pgsql {
-    # ./configure #--enable-cassert --enable-debug CFLAGS="-ggdb -Og -g3 -fno-omit-frame-pointer" : 'uncomment if you haver never compiled it before.'
+    # ./configure # 'uncomment if you haver never compiled it before.'
     make || error_quit "make"
     sudo make install || error_quit "make install"
 }
-
 function run_pgsql {
     rm -rR /usr/local/pgsql/data
     mkdir /usr/local/pgsql/data
@@ -88,11 +78,96 @@ function run_pgsql {
 EOSU
 }
 
+function run_pgsql_timed {
+    rm -rR /usr/local/pgsql/data
+    mkdir /usr/local/pgsql/data
+    chown postgres /usr/local/pgsql/data
+    setfacl -m u:postgres:rwx "${SQL_SCRIPT}"
+    su postgres << EOSU
+    echo "--- init"
+    /usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data || exit
+    echo "--- pg_crl start"
+    /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data start  || exit
+    echo "--- createdb"
+    /usr/local/pgsql/bin/createdb sushi
+    echo "--- psql w/ script"
+    echo ""
+    echo ""
+    echo "" 
+    /usr/local/pgsql/bin/psql -d sushi -f '${SQL_INIT_SCRIPT}'
+EOSU
+
+    start_timer
+    su postgres << EOSU
+    /usr/local/pgsql/bin/psql -d sushi -f '${SQL_VACUUM_SCRIPT}'
+EOSU
+    stop_timer
+    echo ''
+    echo "--- BENCHMARK: VACUUM TIME:"
+    elapsed_time
+    echo ''
+
+    start_timer
+    su postgres << EOSU
+    /usr/local/pgsql/bin/psql -d sushi -f '${SQL_SELECT_SCRIPT}'
+EOSU
+    stop_timer
+    echo ''
+    echo "--- BENCHMARK: SELECT TIME:"
+    elapsed_time
+    echo ''
+
+    start_timer
+    su postgres << EOSU
+EOSU
+    stop_timer
+    echo ''
+    echo "--- BENCHMARK: APPROX COMMAND TIME:"
+    elapsed_time
+    echo ''
+
+    su postgres << EOSU
+    echo ""
+    echo ""
+    echo "--- drop"
+    /usr/local/pgsql/bin/dropdb sushi
+    echo "--- stop"
+    /usr/local/pgsql/bin/pg_ctl stop  -D /usr/local/pgsql/data -m fast || exit
+    sleep 2
+EOSU
+}
+
+function main {
+    current_path=$(pwd)
+    if [ $# -eq 2 ] 
+    then
+        cd "${RAMEN_PATH}"
+        make_pgsql || error_quit "make"
+        
+        if [[ -d ${SQL} ]]; then
+            SQL_INIT_SCRIPT=${SQL}'/INIT.sql'
+            SQL_VACUUM_SCRIPT=${SQL}'/VACUUM.sql'
+            SQL_SELECT_SCRIPT=${SQL}'/SELECT.sql'
+            run_pgsql_timed || error_quit "run-timed"
+        elif [[ -f ${SQL} ]]; then
+            SQL_SCRIPT=${SQL}
+            run_pgsql || error_quit "run"
+        else
+            echo "--- INCORRECT FILE ---"
+            echo ${SQL}
+            echo "--- INCORRECT FILE ---"
+        fi
+        cd "${current_path}"
+    fi
+}
+
 check_root "${@}"
 check_arguments "${@}"
 
-SQL_SCRIPT="${1}"
-RAMEN_PATH="${2}"
+RAMEN_PATH="${1}"
+SQL="${2}"
+echo ${RAMEN_PATH}
+echo ${SQL}
 OFFICIAL_PATH="${3}"
 
 main "${@}"
